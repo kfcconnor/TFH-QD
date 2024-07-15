0335215845fea870514a7023b0a7d37a2f8c9ec9
AGEdraws <157108653+AGEdraws@users.noreply.github.com>
2024-07-15 20:26:37 +0200

    latest update to anne

--- These Final Hours - QD/Contents/mods/These Final Hours - Quest Data/media/lua/shared/NPCPackA.lua
+++ These Final Hours - QD/Contents/mods/These Final Hours - Quest Data/media/lua/shared/NPCPackA.lua
@@ -84,7 +84,7 @@ local medicalMerchant = {
 local bartender = {
     name = "bartender_normal",
     script = "Base.FemaleFolk",
-    haircut = { "HatCurly", {0.67,0.48,0.22} },
+    haircut = { "GrungeyBehindEars", {0.67,0.48,0.22} },
     clothes = {
 		{ "Spongie.Shirt_QuiltedROLL", {0.20,0,1} },
 		{ "Socks_Long", {0.26,0.65,0.35} },
@@ -122,20 +122,19 @@ local rg_guard_1 = {
     }
 };
 
-local SMAdminstrator = {
-    name = "SMAdminstrator_normal",
+local smadmin = {
+    name = "smadmin_normal",
     script = "Base.FemaleFolk",
-    haircut = { "Longcurly", {0,0,0} },
+    haircut = { "F_KotonePonytail", {0.21,0.16,0.10} },
     clothes = {
-		"Base.Hat_HardHat",
-		{ "Base..Tshirt_whiteLongsleveSleeveTINT", {0.10,0.07,0.07} },
-		{ "Socks_Long", {0.26,0.65,0.35} },
-		"Base.Trousers_Fireman",
-		"Base.vestHighViz",
-		"Base.CBX_BOOT_1"
+		"Hat_SurgicalMask_Blue",
+		{ "Tshirt_PoloTINT", {0.48,0.31,0.70} },
+		{ "Shirt_FormalTINT", {0.85,0.43,0.37} },
+		{ "Socks_Ankle", {0.08,0.07,0.08} },
+		"Trousers_SuitTEXTURE",
+		{"Shoes_Random", 0}
     }
 };
-
 
 table.insert(MFManager.templates, sally_1);
 table.insert(MFManager.templates, weaponMerchant);
@@ -146,4 +145,4 @@ table.insert(MFManager.templates, medicalMerchant);
 table.insert(MFManager.templates, bartender);
 table.insert(MFManager.templates, quartermaster);
 table.insert(MFManager.templates, rg_guard_1);
-table.insert(MFManager.templates, SMAdminstrator_normal);
+table.insert(MFManager.templates, smadmin);

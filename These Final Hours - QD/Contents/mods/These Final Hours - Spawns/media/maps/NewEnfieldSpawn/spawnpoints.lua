2bf1de0615b570ce0244cb58e955cf447273d54d
Lyons <connorthompson1995@hotmail.com>
2024-08-16 18:36:07 +0100

    Moved new enfield Spawn and added trade items to NET

--- These Final Hours - QD/Contents/mods/These Final Hours - Spawns/media/maps/NewEnfieldSpawn/spawnpoints.lua
+++ These Final Hours - QD/Contents/mods/These Final Hours - Spawns/media/maps/NewEnfieldSpawn/spawnpoints.lua
@@ -1,7 +1,7 @@
 function SpawnPoints()
 return {
 		unemployed = {
-			{ worldX = 28, worldY = 31, posX = 184, posY = 177, posZ = 0 }
+			{ worldX = 28, worldY = 31, posX = 181, posY = 167, posZ = 0 }
   }
 }
 end
